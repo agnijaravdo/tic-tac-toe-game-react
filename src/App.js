484642fe7fd741c83019a3d7f557d5ae93fa3b4f
agnijaravdo <41111594+agnijaravdo@users.noreply.github.com>
2024-02-06 23:13:39 +0000

    add keys to each cell element

--- src/App.js
+++ src/App.js
@@ -128,13 +128,17 @@ function App() {
       <br />
       <div>
         {board.map((row, rowIndex) => (
-          <div className="row">
+          <div
+            key={rowIndex}
+            className="row"
+          >
             {row.map((column, columnIndex) => (
               <div
                 onClick={() => {
                   selectCell(rowIndex, columnIndex);
                 }}
                 style={{ cursor: "pointer" }}
+                key={columnIndex}
                 className="column"
               >
                 {board[rowIndex][columnIndex]}
