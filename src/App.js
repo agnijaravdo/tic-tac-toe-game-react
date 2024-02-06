7c60a262536a95cf7c0753c2a801b54561fbd519
agnijaravdo <41111594+agnijaravdo@users.noreply.github.com>
2024-02-06 18:56:28 +0000

    render board and handleClick on cell

--- src/App.js
+++ src/App.js
@@ -1,10 +1,55 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
+  const [board, setBoard] = useState([]);
+  const [boardSize, setBoardSize] = useState(3);
+  const [player, setPlayer] = useState("0");
+
+  useEffect(() => {
+    const row = new Array(boardSize).fill(null);
+
+    const tempBoard = [];
+
+    for (let i = 0; i < boardSize; i++) {
+      tempBoard.push([...row]);
+    }
+    setBoard(tempBoard);
+  }, []);
+
+  function handleOnClick(rowIndex, columnIndex) {
+    console.log(`rowIndex ${rowIndex}, columnIndex ${columnIndex} `);
+    if (!board[rowIndex][columnIndex]) {
+      let nextPlayer = player === "X" ? "0" : "X";
+      setPlayer(nextPlayer);
+      const boardCopy = [...board];
+      boardCopy[rowIndex][columnIndex] = nextPlayer;
+      setBoard(boardCopy);
+    }
+  }
+
   return (
-    <div>
+    <div className="App">
       <h1>Tic Tac Toe Game</h1>
+      <div>
+        {board.map((row, rowIndex) => (
+          <div className="row">
+            {row.map((column, columnIndex) => (
+              <div
+                onClick={() => {
+                  handleOnClick(rowIndex, columnIndex);
+                }}
+                style={{ cursor: "pointer" }}
+                className="column"
+              >
+                {board[rowIndex][columnIndex]}
+              </div>
+            ))}
+          </div>
+        ))}
+      </div>
+      <br />
+      <div>PLAYER CLICKED ON: </div>
     </div>
   );
 }
